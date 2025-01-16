eff8e27b3b301a54169e3b244c4570498a05ed27
Chanudhi <aluchanu29@gmail.com>
2025-01-16 23:40:30 +0530

    updates

--- todo_list/test/widget_test.dart
+++ todo_list/test/widget_test.dart
@@ -1,14 +1,9 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
+// Import the necessary Flutter packages
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:todo_list/main.dart';
+// Import the main.dart file from the todo_list package
+import '../lib/main.dart'; // Replace '../lib/main.dart' with the correct path to your main.dart file
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
@@ -28,3 +23,4 @@ void main() {
     expect(find.text('1'), findsOneWidget);
   });
 }
+
